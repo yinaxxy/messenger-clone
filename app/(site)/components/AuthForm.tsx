17da2f9022275e36796da45cd8707f0694714520
NABAO <xyinaxy@gmail.com>
2023-12-04 17:26:15 -0800

    register and login functions

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,12 +1,16 @@
 // This is a client Component, which means it is interactive. So need to have 'use client'
 'use client';
 
+import axios from 'axios';
+
 import { useCallback, useState } from 'react';
 import {
   useForm,
   FieldValues,
   SubmitHandler } from 'react-hook-form';
 import { BsGithub, BsGoogle } from 'react-icons/bs';
+import { toast } from 'react-hot-toast';
+import { signIn } from 'next-auth/react';
 
 import Input from '@/app/components/inputs/Input';
 import Button from '@/app/components/Button';
@@ -46,10 +50,27 @@ const AuthForm = () => {
 
     if (variant === 'REGISTER') {
       //axios register
+      axios.post('/api/register', data)
+      .catch(() => toast.error('Something went wrong!'))
+      .finally(() => setIsLoading(false))
     }
 
-    if (variant == 'LOGIN') {
+    if (variant === 'LOGIN') {
       // NextAuth SignIn
+      signIn('credentials', {
+        ...data,
+        redirect: false
+      })
+      .then((callback) => {
+        if (callback?.error) {
+          toast.error('Invalid credentials');
+        }
+
+        if (callback?.ok && !callback?.error) {
+          toast.success('Logged in!')
+        }
+      })
+      .finally(() => setIsLoading(false));
     }
   }
 
@@ -57,6 +78,17 @@ const AuthForm = () => {
     setIsLoading(true);
 
     // NextAuth Social Sign In
+    signIn(action, { redirect:false })
+    .then((callback) => {
+      if (callback?.error) {
+        toast.error('Invalid Credentials');
+      }
+
+      if (callback?.ok && !callback?.error) {
+        toast.success('Logged in!')
+      }
+    })
+    .finally(() => setIsLoading(false));
   }
 
   return (
