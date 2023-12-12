2b38ce7b705511453f0356e179edad916cb8fc09
NABAO <xyinaxy@gmail.com>
2023-12-12 15:53:05 -0800

    retrieve current session and user and pass as prop to sidebar

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -4,11 +4,19 @@ import { useState } from 'react';
 
 import useRoutes from "@/app/hooks/useRoutes";
 import DesktopItem from './DesktopItem';
+import { User } from '@prisma/client';
 
-const DesktopSidebar = () => {
+interface DesktopSidebarProps {
+  currentUser: User
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
+  currentUser
+}) => {
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState(false);
 
+  console.log( {currentUser });
   return (
     <div
       className="
--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,12 +1,17 @@
 import DesktopSidebar from "./DesktopSidebar";
 import MobileFooter from "./MobileFooter";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
 
 async function Sidebar({ children }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
+
   return (
     <div className="h-full">
-      <DesktopSidebar />
+      {/* add ! after currentUser to enable currentUser to be null or undefined */}
+      <DesktopSidebar currentUser={currentUser!}/>
       <MobileFooter />
       <main className="lg:pl-20 h-full">
         {children}
